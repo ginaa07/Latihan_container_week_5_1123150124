fbc902bb7c9bb5ef34106ed81b1edbf6e4c9ae85
ginaa07 <1123150124@global.ac.id>
2025-10-24 13:19:27 +0700

    Constructor of Container Class

---
+++ android/app/src/main/kotlin/com/example/lat_container_week5/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lat_container_week5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+//step 1 void main
+void main() {
+  runApp(MyApp());
+}
+
+//step bikin statelsswiget untuk material app
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
